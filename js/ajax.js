49947ca713b06871ad67e3274e29a4d93aed4262
Michael Taylor <michael.taylor@webdesignstudiouk.com>
2017-07-30 20:21:34 +0100

    [added] correct url for Ajax

--- js/ajax.js
+++ js/ajax.js
@@ -9,7 +9,7 @@ jQuery(function($) {
         var type_id = $("#ajax_stored_information_table").data('type');
         console.log(type_id);
         $.ajax({
-            url: window.location.origin + "/admin/options/ajax/stored-infomation/" + type_id,
+            url: window.location.origin + "/hosting/ebn_dev/" + "admin/options/ajax/stored-infomation/" + type_id,
             type: 'GET'
         }).done(function (data) {
             console.log(data);
