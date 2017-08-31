f923f69e5c990cfb0f4ab69f8d310e89e9c84276
Michael Taylor <michael.taylor@unionroom.com>
2017-08-31 12:20:12 +0100

    [fixed] issue with requesting prospects

--- resources/views/prospects/prospects.blade.php
+++ resources/views/prospects/prospects.blade.php
@@ -26,11 +26,11 @@
 		@endpermission
 		@permission('prospectsystem.request')
 			{{--@role('admin')--}}
-				<li><a href="{{route('prospects.request')}}">Request Prospect</a></li>
+				{{--<li><a href="{{route('prospects.request')}}">Request Prospect</a></li>--}}
 			{{--@else--}}
-				{{--<li><a href="{{route('prospects.request_agent')}}">Request Prospect - A prospect will be requested on click.--}}
-						{{--<span class="badge badge-warning"> {{App\Models\Prospects::where('campaign_id', 22)->where('user_id', 100)->count()}}--}}
-				{{--</span></a></li>--}}
+				<li><a href="{{route('prospects.request_agent')}}">Request Prospect - A prospect will be requested on click.
+						<span class="badge badge-warning"> {{App\Models\Prospects::where('campaign_id', 22)->where('user_id', 100)->whereIn('lead_type', array(2,3))->count()}}
+				</span></a></li>
 			{{--@endrole--}}
 		@endpermission
 
