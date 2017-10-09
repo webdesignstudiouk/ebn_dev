c3fa5efe5936e2a9c16615b3b65f30d7b7333220
Michael Taylor <michael.taylor@unionroom.com>
2017-10-09 20:28:37 +0100

    [fixed] if callback is hard deleted

--- resources/views/notifications/Callback_Create.php
+++ resources/views/notifications/Callback_Create.php
@@ -2,17 +2,17 @@
 $type = "success";
 $icon = "plus";
 $callback = \App\Models\ProspectsCallbacks::withTrashed()->find($notification->data['created_callback']);
-$prospect = $callback->prospect;
-if($prospect){
-	$link = route('prospects.edit', $prospect->id);
-	$message = "created a callback for <strong>". $prospect->company.'</strong>';
-
-	$content = "<blockquote>".$callback->note."</blockquote>";
-	$content .= "<ul>";
-	$content .= "<li>Callback Date:  ".\Carbon\Carbon::createFromFormat( 'Y-m-d', $callback->callbackDate )->format('d/m/Y')."</li>";
-	$content .= "<li>Callback Time:  ".$callback->callbackTime."</li>";
-	$content .= "</ul>";
-}else{
-	$message = "created a callback";
+if($callback) {
+	$prospect = \App\Models\Prospects::withTrashed()->find( $callback->prospect_id );
+	$content  = '';
+	if ( $prospect ) {
+		$message = "created a callback for <strong>" . $prospect->company . '</strong>';
+		$content .= "<blockquote>" . $callback->note . "</blockquote>";
+		$content .= "<ul>";
+		$content .= "<li>Callback Date:  " . \Carbon\Carbon::createFromFormat( 'Y-m-d', $callback->callbackDate )->format( 'd/m/Y' ) . "</li>";
+		$content .= "<li>Callback Time:  " . $callback->callbackTime . "</li>";
+		$content .= "</ul>";
+	} else {
+		$message = "created a callback";
+	}
 }
-
