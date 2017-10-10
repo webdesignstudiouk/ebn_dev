8ab8299bbf3ff29ec6ac39f357e0c39d72b597c3
Michael Taylor <michael.taylor@unionroom.com>
2017-10-10 18:56:29 +0100

    [fixed] view single delete info

--- resources/views/notifications/Prospect_Delete.php
+++ resources/views/notifications/Prospect_Delete.php
@@ -3,19 +3,15 @@
     $icon = "trash";
     if(isset($notification->data['deleted_prospects']) && count($notification->data['deleted_prospects']) > 1){
         $message = "deleted ". count($notification->data['deleted_prospects'])." prospects";
-        $content = "<ul>";
-
-	    foreach ( $notification->data['deleted_prospects'] as $deleted_prospect ) {
-		    $content.="<li>".$deleted_prospect.' - '.\App\Models\Prospects::withTrashed()->find($deleted_prospect)->company."</li>";
-        }
-	    $content .= "</ul>";
     }else{
-        $prospect = \App\Models\Prospects::withTrashed()->find($notification->data['deleted_prospects'][0]);
-        if($prospect){
-            $message = "deleted a prospect: ".$prospect->company;
-        }else{
-            $message = "deleted a prospect";
-        }
+	    $message = "deleted a prospect";
     }
 
+    if(isset($notification->data['deleted_prospects']) && count($notification->data['deleted_prospects']) > 0) {
+	    $content = "<ul>";
+	    foreach ( $notification->data['deleted_prospects'] as $deleted_prospect ) {
+		    $content .= "<li>" . $deleted_prospect . ' - ' . \App\Models\Prospects::withTrashed()->find( $deleted_prospect )->company . "</li>";
+	    }
+	    $content .= "</ul>";
+    }
 
