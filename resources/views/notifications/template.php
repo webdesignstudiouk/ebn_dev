54b6c8c6227b4a9cb0a55d53f29a931372ce5965
Michael Taylor <michael.taylor@unionroom.com>
2017-10-10 18:34:49 +0100

    [fixed] timestamp for notifications

--- resources/views/notifications/template.php
+++ resources/views/notifications/template.php
@@ -2,7 +2,7 @@
 if ( !$skip ) {
 	if ( $timeline ) {
 		echo "<li>
-	    <time class='cbp_tmtime' datetime='2017-10-09T03:45'><span>" . \Carbon\Carbon::createFromFormat( 'Y-m-d H:i:s', $notification->created_at )->format( 'H:i A' ) . "</span> <span>" . \Carbon\Carbon::createFromFormat( 'd/m/Y', $notification->created_at->format( 'd/m/Y' ) )->diffForHumans() . "</span></time>
+	    <time class='cbp_tmtime' datetime='2017-10-09T03:45'><span>" . \Carbon\Carbon::createFromFormat( 'Y-m-d H:i:s', $notification->created_at )->format( 'H:i A' ) . "</span> <span>" . \Carbon\Carbon::createFromTimeStamp(strtotime($notification->created_at) )->diffForHumans() . "</span></time>
 	    <div class='cbp_tmicon timeline-bg-" . ( isset( $type ) ? $type : '' ) . "'>
 	        <i class='fa-" . ( isset( $icon ) ? $icon : '' ) . "'></i>
 	    </div>
@@ -19,7 +19,7 @@ if ( !$skip ) {
 		<a href='" . ( isset( $link ) ? $link : '' ) . "'> 
 			<i class='fa-" . ( isset( $icon ) ? $icon : '' ) . "'></i> 
 			<span class='line'>" . ( $notification->read_at ? '' : '<strong>' ) . ( isset( $prefix ) ? $prefix : '' ) . " " . ( isset( $message ) ? $message : '' ) . ( $notification->read_at ? '' : '</strong>' ) . "</span> 
-			<span class='line small time'>" . \Carbon\Carbon::createFromFormat( 'd/m/Y', $notification->created_at->format( 'd/m/Y' ) )->diffForHumans() . "</span> 
+			<span class='line small time'>" . \Carbon\Carbon::createFromTimeStamp(strtotime( $notification->created_at))->diffForHumans() . "</span> 
 		</a> 
 	</li>
 	";
