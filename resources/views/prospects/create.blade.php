3413fa368bd23817a3859900278a26fceee82cca
michael-unionroom <michael.taylor@unionroom.com>
2018-02-20 18:51:43 +0000

    [removed] nabber from prospects create

--- resources/views/prospects/create.blade.php
+++ resources/views/prospects/create.blade.php
@@ -4,24 +4,5 @@
 @section('page-description', 'Create a prospect.')
 
 @section('content')
-<nav class="navbar navbar-default">
-	<ul class="nav navbar-nav">
-		<li><a href="{{url('admin/prospects')}}">Prospects
-			<span class="badge badge-info">{{App\Models\Prospects::where('type_id', 1)->where('user_id', Auth::user()->id)->orderBy('company', 'desc')->count()}}</span></a>
-		</li>
-		<li><a href="{{url('admin/prospects_2')}}">Prospects 2
-			<span class="badge badge-info">{{App\Models\Prospects::where('type_id', 2)->where('user_id', Auth::user()->id)->orderBy('company', 'desc')->count()}}</span></a>
-		</li>
-		<li><a href="{{url('admin/clients')}}">Clients
-			<span class="badge badge-info">{{App\Models\Prospects::where('type_id', 3)->where('user_id', Auth::user()->id)->orderBy('company', 'desc')->count()}}</span></a>
-		</li>
-		<li><a href="{{route('prospects.create')}}">Create Prospect</a></li>
-		@role('admin')
-		<li><a href="{{route('prospects.request')}}">Request Prospect</a></li>
-		@else
-			<li><a href="{{route('prospects.request_agent')}}">Request Prospect <span class="badge badge-warning">A prospect will be requested on click.</span></a></li>
-		@endrole
-	</ul>
-</nav>
 	{!! form($form) !!}
 @endsection
